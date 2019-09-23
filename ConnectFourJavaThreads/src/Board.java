144bb3ca09f37f6c11f8d54b4ecb3e6b3b33c9e1
johphil <johphilencarnacion@gmail.com>
2019-09-23 11:05:02 +0800

    board

---
+++ ConnectFourJavaThreads/src/Board.java
@@ -0,0 +1,33 @@
+
+public class Board implements Runnable {
+	int BOARD_COLS = 7;
+	int BOARD_ROWS = 6;
+	
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public void printBoard(char[] board)
+	{
+		int col;
+		System.out.println("\n    ****Connect Four****\n");
+		for(int row = 0; row < BOARD_ROWS; row++)
+	    {
+	        for(col = 0; col < BOARD_COLS; col++)
+	        {
+	        	System.out.print(String.format("| %c ",  board[BOARD_COLS * row + col]));
+	        }
+	        System.out.println("|");
+	        System.out.println("-----------------------------");
+	    }
+		System.out.println("  1   2   3   4   5   6   7\n");
+	}
+	
+	public void clearScreen()
+	{
+		System.out.print("\033[H\033[2J");  
+	    System.out.flush();  
+	}
+}	
---
+++ ConnectFourJavaThreads/src/main.java
@@ -0,0 +1,10 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		char[] board = new char[6*7];
+		Board b = new Board();
+		b.printBoard(board);
+	}
+
+}
